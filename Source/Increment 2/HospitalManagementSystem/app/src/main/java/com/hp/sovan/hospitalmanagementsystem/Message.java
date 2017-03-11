933c1c1d5d95f121d0b18d7c1701711895a874cc
sovang5 <sovang5@gmail.com>
2017-03-11 10:41:19 +0530

    Increment 2 update

---
+++ Source/Increment 2/HospitalManagementSystem/app/src/main/java/com/hp/sovan/hospitalmanagementsystem/Message.java
@@ -0,0 +1,12 @@
+package com.hp.sovan.hospitalmanagementsystem;
+
+import android.content.Context;
+import android.widget.Toast;
+
+
+public class Message {
+    public static void message(Context context,String message)
+    {
+        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+    }
+}
